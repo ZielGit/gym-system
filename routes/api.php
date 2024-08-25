9945ad7c9f4858482c153bedc61843358e6b22f2
Frans <frans_421_12@hotmail.com>
2024-08-25 16:03:50 -0500

    fix: Route login post

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ return function ($router) {
     $router->addRoute('GET', '/home', [HomeController::class, 'index']);
 
     // Auth
-    $router->addRoute('GET', '/login', [AuthController::class, 'login']);
+    $router->addRoute('POST', '/login', [AuthController::class, 'login']);
     $router->addRoute('GET', '/logout', [AuthController::class, 'logout']);
 };
