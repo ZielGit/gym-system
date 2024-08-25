4dcc5a264bf58f30a19f19d8f588a3f62de06af7
Frans <frans_421_12@hotmail.com>
2024-08-25 16:07:47 -0500

    feat: Login and index view

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Controllers\HomeController;
 
 return function ($router) {
     $router->addRoute('GET', '/', [HomeController::class, 'index']);
+    $router->addRoute('GET', '/login', [HomeController::class, 'login']);
 };
