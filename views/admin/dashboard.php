ac97c1a4649d20cb4b525aa5b0c3f090f916dc74
Frans <frans_421_12@hotmail.com>
2024-10-10 12:05:37 -0500

    feat: Dashboard view

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -14,32 +14,20 @@ startSection('title'); ?>
                 <div class="card-body">
                     <div class="d-flex flex-column flex-md-row align-items-center justify-content-between">
                         <div class="statistics-item">
-                            <p>
-                                <i class="icon-sm fa fa-user mr-2"></i>
-                                Empleados
-                            </p>
-                            <!-- <h2><?php echo $data['usuarios']['total']; ?></h2> -->
+                            <p><i class="icon-sm fa fa-user mr-2"></i> Usuarios</p>
+                            <h2 id="users"></h2>
                         </div>
                         <div class="statistics-item">
-                            <p>
-                                <i class="icon-sm fas fa-users mr-2"></i>
-                                Clientes
-                            </p>
-                            <!-- <h2><?php echo $data['clientes']['total']; ?></h2> -->
+                            <p><i class="icon-sm fas fa-users mr-2"></i> Clientes</p>
+                            <h2 id="customers"></h2>
                         </div>
                         <div class="statistics-item">
-                            <p>
-                                <i class="icon-sm fas fa-list-alt mr-2"></i>
-                                Planes
-                            </p>
-                            <!-- <h2><?php echo $data['planes']['total']; ?></h2> -->
+                            <p><i class="icon-sm fas fa-list-alt mr-2"></i> Planes</p>
+                            <h2 id="plans"></h2>
                         </div>
                         <div class="statistics-item">
-                            <p>
-                                <i class="icon-sm fas fa-check-circle mr-2"></i>
-                                Entrenadores
-                            </p>
-                            <!-- <h2><?php echo $data['entrenador']['total']; ?></h2> -->
+                            <p><i class="icon-sm fas fa-check-circle mr-2"></i> Entrenadores</p>
+                            <h2 id="coaches"></h2>
                         </div>
                     </div>
                 </div>
@@ -54,11 +42,11 @@ startSection('title'); ?>
                     <div class="card-title">
                         <h6>Reporte Grafico de Ingreso por Mes
                             <select id="year" class="float-end" onchange="actualizarGrafico()">
-                                <!-- <?php
+                                <?php
                                 $fecha = date('Y');
                                 for ($i = 2021; $i <= $fecha; $i++) { ?>
-                                    <option value="<?php echo $i; ?>" <?php echo ($i == $fecha) ? 'selected' : ''; ?>><?php echo $i; ?></option>
-                                <?php } ?> -->
+                                <option value="<?php echo $i; ?>" <?php echo ($i == $fecha) ? 'selected' : ''; ?>><?php echo $i; ?></option>
+                                <?php } ?>
                             </select>
                         </h6>
                     </div>
@@ -78,7 +66,132 @@ startSection('title'); ?>
         </div>
     </div>
 <?php endSection(); ?>
+<?php startSection('scripts'); ?>
+    <script>
+        $.ajax({
+            type: "get",
+            url: `${api_admin_url}/dashboard`,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+            dataType: "json",
+            success: function (response) {
+                $('#users').text(response.users);
+                $('#customers').text(response.customers);
+                $('#plans').text(response.plans);
+                $('#coaches').text(response.coaches);
+            }
+        });
 
+        if (document.getElementById('ProductosVendidos')) {
+            actualizarGrafico();
+            ventasDia();
+        }
+
+        function actualizarGrafico() {
+            const anio = document.getElementById('year').value;
+            let ctx = document.getElementById('ProductosVendidos').getContext('2d');
+            if (myChart) {
+                myChart.destroy();
+            }
+            $.ajax({
+                type: "get",
+                url: `${api_admin_url}/dashboard/products-sold/${user_id}`,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                data: {
+                    anio
+                },
+                dataType: "json",
+                success: function (response) {
+                    myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+                            datasets: [{
+                                label: 'Ingreso por Mes',
+                                data: [response.ene, response.feb, response.mar, response.abr, response.may, response.jun, response.jul, response.ago, response.sep, response.oct, response.nov, response.dic],
+                                backgroundColor: [
+                                    'rgb(255, 202, 240)'
+                                ]
+                            }]
+                        },
+                        options: {
+                            indexAxis: 'x',
+                            elements: {
+                                bar: {
+                                    borderWidth: 2,
+                                }
+                            },
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    position: 'top',
+                                },
+                                title: {
+                                    display: false,
+                                    text: 'Pagos por Mes'
+                                }
+                            }
+                        },
+                    });
+                }
+            });
+        }
+
+        function ventasDia() {
+            let ctx = document.getElementById('ventaDia').getContext('2d');
+            $.ajax({
+                type: "get",
+                url: `${api_admin_url}/dashboard/sales/${user_id}`,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                dataType: "json",
+                success: function (response) {
+                    let nombre = [];
+                    let cantidad = [];
+                    for (let i = 0; i < response.length; i++) {
+                        nombre.push(response[i].name);
+                        cantidad.push(response[i].total);
+                    }
+                    let my_Chart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: nombre,
+                            datasets: [{
+                                label: 'Ingreso por Día',
+                                data: cantidad,
+                                backgroundColor: [
+                                    'rgb(200, 0, 00)'
+                                ]
+                            }]
+                        },
+                        options: {
+                            indexAxis: 'x',
+                            elements: {
+                                bar: {
+                                    borderWidth: 2,
+                                }
+                            },
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    position: 'top',
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Ventas por Día'
+                                }
+                            }
+                        },
+                    });
+                }
+            });
+        }
+    </script>
+<?php endSection(); ?>
 <?php
 
 // Incluir el layout
