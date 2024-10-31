8021f071b26af28a4c7d0da679a98c50e95c6884
Frans <frans_421_12@hotmail.com>
2024-10-30 19:46:18 -0500

    chore: change gmail

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
             <div class="row align-items-center">
                 <div class="col-lg-6">
                     <ul class="top-bar-info list-inline-item pl-0 mb-0">
-                        <li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>info@sistemasfree.com</a></li>
+                        <li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>support@gmail.com</a></li>
                         <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Address Ta-134/A, New York, USA </li>
                     </ul>
                 </div>
